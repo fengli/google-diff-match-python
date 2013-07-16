6397463d51f9d79c5ecceeab1a7f407e8b593afa
okidogi <okidogii@gmail.com>
2013-07-16 22:13:09 +0200

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+VERSION='1.0'
+
+long_description = open('README.md').read()
+
+setup(
+    name='gdiff',
+    version=VERSION,
+    packages=['gdiff', 
+              ],
+    description='google diff match library.'
+    long_description=long_description,
+    author='neil fraser @ google',
+    author_email='neil.fraser@gmail.com',
+    license='Apache License v2.0',
+    url='https://github.com/fengli/google-diff-match-python',
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
